a535203d5d007b704c92ec22fc3e3a3632db5d82
Omar Bouattour <oommaarr10111@gmail.com>
2024-11-14 22:48:41 +0100

    added triangular and band matrices

--- src/Algorithms/Algorithm.js
+++ src/Algorithms/Algorithm.js
@@ -1,4 +1,4 @@
-export function gaussSeidel(A, b, x = [], tolerance = 1e-10, maxIterations = 100) {
+ function gaussSeidel(A, b, x = [], tolerance = 1e-10, maxIterations = 100) {
     const n = A.length;
     if (x.length === 0) x = new Array(n).fill(0);
     for (let k = 0; k < maxIterations; k++) {
@@ -21,3 +21,63 @@ export function gaussSeidel(A, b, x = [], tolerance = 1e-10, maxIterations = 100
     console.log("Did not converge within the maximum number of iterations.");
     return null;
 }
+
+function generateLowerTriangularMatrix(n) {
+    const matrix = Array.from({ length: n }, () => Array(n).fill(0));
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j <= i; j++) {
+            matrix[i][j] = Math.floor(Math.random() * 100);
+        }
+    }
+
+    return matrix;
+}
+
+ function generateUpperTriangularMatrix(n) {
+    const matrix = Array.from({ length: n }, () => Array(n).fill(0));
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            matrix[i][j] = Math.floor(Math.random() * 100);
+        }
+    }
+
+    return matrix;
+}
+
+function generateLowerHalfBandMatrix(n, b) {
+    const matrix = Array.from({ length: n }, () => Array(n).fill(0));
+
+    for (let i = 0; i < n; i++) {
+        for (let j = Math.max(0, i - b); j <= i; j++) {
+            matrix[i][j] = Math.floor(Math.random() * 100); // Random number between 0 and 9
+        }
+    }
+
+    return matrix;
+}
+
+function generateUpperHalfBandMatrix(n, b) {
+    const matrix = Array.from({ length: n }, () => Array(n).fill(0));
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i ; j < Math.min(i + b + 1, n); j++) {
+            matrix[i][j] = Math.floor(Math.random() * 100); // Random number between 0 and 9
+        }
+    }
+
+    return matrix;
+}
+function generateBandMatrix(n, b) {
+    const matrix = Array.from({ length: n }, () => Array(n).fill(0));
+
+    for (let i = 0; i < n; i++) {
+        for (let j = Math.max(0, i - b) ; j < Math.min(i + b + 1, n); j++) {
+            matrix[i][j] = Math.floor(Math.random() * 100); // Random number between 0 and 9
+        }
+    }
+
+    return matrix;
+}
+console.log(generateBandMatrix(3, 1));
