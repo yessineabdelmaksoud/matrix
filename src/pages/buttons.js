7600b7a5a66e3c8429fc7924960f9eb00010c96a
Mohamed Ali Abid <mohamedali.abid260104@gmail.com>
2024-12-04 21:55:46 +0100

    Add smooth scrolling on calculate button click and clean up package-lock.json

--- src/pages/buttons.js
+++ src/pages/buttons.js
@@ -3,6 +3,11 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 function buttons({ method, size, handleDownloadMatrix, displayMatrix, calculate }) {
+    const handleCalculateClick = () => {
+        calculate();
+        window.scrollTo({ top: document.body.scrollHeight / 2, behavior: 'smooth' });
+    };
+
     return (
         <div className="text-center mt-4">
             {method === 'random' && size > 30 ? (
@@ -14,7 +19,7 @@ function buttons({ method, size, handleDownloadMatrix, displayMatrix, calculate 
                     Show Matrix
                 </Button>
             )}
-            <Button className="btn-calculate mx-2" onClick={calculate}>Calculate</Button>
+            <Button className="btn-calculate mx-2" onClick={handleCalculateClick}>Calculate</Button>
         </div>
     );
 }
